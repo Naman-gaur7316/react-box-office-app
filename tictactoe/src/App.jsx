5c5b42854c59a3d40527ed2cdfca6d6fbfc883dc
Naman <namangaur4102@gmail.com>
2022-06-29 23:26:00 +0530

    init commit

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/function-component-definition */
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+  );
+}
+
+export default App;
