f346d0ad2c66519055cb9a15ed96397048d20ae6
Naman <namangaur4102@gmail.com>
2022-07-01 11:07:30 +0530

    created board and square component

---
+++ tictactoe/src/components/Square.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react'
+
+const Square = ({ value }) => {
+  return <button type='button'>{value}</button>
+}
+
+export default Square
